7fe824474657b956cea768776212c17dad9cfcb7
leodevo <git>
2019-02-25 19:04:32 +0100

    removing useless log

--- db/mongoose.js
+++ db/mongoose.js
@@ -6,7 +6,6 @@ mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true }).then(() => 
   console.log('connected to mongoDB service')
 }, (err) => {
   console.log(err)
-  console.log('Could not connect to DB, make sure a DB application is running')
 })
 
 module.exports = { mongoose }
